44b03108528d004f585e8eb9f796ee46fb7d4a9d
Ryan <muktiryan@gmail.com>
2020-08-18 13:43:31 +0700

    change datasource

--- src/main/java/com/talentnxg/talentnxgapi/configs/DatasourceConfig.java
+++ src/main/java/com/talentnxg/talentnxgapi/configs/DatasourceConfig.java
@@ -9,13 +9,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DatasourceConfig {
 //	jdbc:postgresql:///<DATABASE_NAME>?cloudSqlInstance=<INSTANCE_CONNECTION_NAME>&socketFactory=com.google.cloud.sql.postgres.SocketFactory&user=<POSTGRESQL_USER_NAME>&password=<POSTGRESQL_USER_PASSWORD>
-	String googleConnectionName= "jdbc:postgresql://google/postgres?cloudSqlInstance=dotted-hulling-275514:us-central1:cloud-tnxg-postgres&socketFactory=com.google.cloud.sql.postgres.SocketFactory&user=postgres&password=p@ssword.1";
+//	String googleConnectionName= "jdbc:postgresql://google/postgres?cloudSqlInstance=dotted-hulling-275514:us-central1:cloud-tnxg-postgres&socketFactory=com.google.cloud.sql.postgres.SocketFactory&user=postgres&password=p@ssword.1";
+	String googleConnectionName= "jdbc:postgresql://google/postgres?cloudSqlInstance=valiant-sandbox-286800:us-central1:cloud-tnxg-postgres&socketFactory=com.google.cloud.sql.postgres.SocketFactory&user=postgres&password=p@ssword.1";
 	@Bean
 	public DataSource dataSource() {
 		return DataSourceBuilder.create()
 //		          .driverClassName("org.postgresql.Driver")
 		          .url(googleConnectionName)
-//		          .url("jdbc:postgresql://35.202.17.127:5432/postgres")
+//		          .url("jdbc:postgresql://34.121.112.120:5432/postgres")
 //		          .url("jdbc:postgresql://localhost:5433/postgres")
 //		          .url("jdbc:postgresql://localhost:5432/newtnxg")
 		          .username("postgres")
